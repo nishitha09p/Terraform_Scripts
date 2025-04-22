c1c92a5be51f397d7fad94b77487491851c2337c
nishitha09p <patel.nishitha@dxc.com>
2025-04-22 16:56:30 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,45 +1,31 @@
 provider "google" {
   project     = "myprojectnishitha13593"
   region      = "us-central1"
-  credentials = "keys.json"
-}
-
-resource "google_compute_network" "jio_network" {
-  name                    = "jio-network"
-}
-resource "google_compute_network" "airtel_network" {
-  name                    = "airtel-network"
-}
-data "google_compute_network" "datasource_airtel_network" {
- name = google_compute_network.airtel_network.name
-}
-
-resource "google_compute_subnetwork" "jio_subnet" {
-  name            = "jio-subnet"
-  ip_cidr_range = "10.2.0.0/24"
-  network       = google_compute_network.jio_network.id  
-  region          = "us-central1"
+  credentials = "c:/terraform/data/count/keys.json"
 }
 
 resource "google_compute_instance" "my_instance" {
-  name         = "my-instance"
-  machine_type = "e2-micro"
-  zone         = "us-central1-c"
+  for_each = {
+    prod = "e2-micro"
+    dev  = "e2-small"
+  }
+
+  name         = "india-${each.key}"
+  machine_type = each.value
+  zone         = "us-central1-b"
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
     }
   }
-  network_interface {
-  
-    subnetwork = google_compute_subnetwork.jio_subnet.id
-    access_config {}
+ network_interface {
+    network      = "default"
+    access_config {} # Request a public IP
   }
-  network_interface {
-  
-    network = data.google_compute_network.datasource_airtel_network.id
-    access_config {}
-  }
-
-
 }
+
+
+
+  
+
