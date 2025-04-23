fdb8ae6d0a57b288163fdc40bfa70e9565605e0a
nishitha09p <patel.nishitha@dxc.com>
2025-04-23 10:35:10 +0530

    Add files via upload

--- var.tf
+++ var.tf
@@ -1,52 +1,39 @@
-variable "project_ID" {
-    type = string
-    
-    description = "Nishitha's Project_ID"
-}
-variable "region" {
-    type = string
-   
-    description = "Google Cloud Provider's region"
-}
-variable "credentials" {
-    type = string
-    
-    description = "Path of Service account key"
-}
-variable "vpc-network" {
-    type = string
-    
-    description = "name of the VPC network"
-}
-variable "my_subnet" {
-    type = string
-    
-    description = "name of the subnet"
-}
-variable "subnet_CIDR" {
-    type = string
-    
-    description = "Range of the subnet CIDR"
-}
-variable "instance_type" {
-    type = string
-   
-    description = "Name of the Instance"
-}
-variable "machine_type" {
-    type        = string
-   
-    description = "machine type of the instance"
+variable "project" {
+  description = "Nishitha's Project_Id"
+  type        = string
 }
 
-variable "instance_zone" {
-    type        = string
-    
-    description = "The zone for the instance"
+variable "region" {
+  description = "region of project"
+  type        = string
 }
-variable "boot_disk_image" {
-    type        = string
-   
-    description = "The image to use for the boot disk"
+
+variable "credentials" {
+  description = "Path of the service account key"
+  type        = string
 }
- 
+
+variable "instance_count" {
+  description = "number of compute instances to be created"
+  type        = number
+}
+
+variable "machine_type" {
+  description = "machine type of the compute instances"
+  type        = string
+}
+
+variable "zone" {
+  description = "zone for the compute instances"
+  type        = string
+}
+
+variable "image" {
+  description = "The boot disk image for the compute instances"
+  type        = string
+}
+
+variable "network" {
+  description = "network for the compute instances"
+  type        = string
+}
